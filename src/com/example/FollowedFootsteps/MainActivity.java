17cdb3c6ba45bfaf4596086dfbf4c379c09f1a0a
unknown <tvw3@nau.edu>
2014-03-31 23:34:11 -0700

    Added navigation drawer

---
+++ src/com/example/FollowedFootsteps/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.followedfootsteps;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends Activity {
+	private String[] mTitles;
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private ActionBarDrawerToggle actionBarDrawerToggle;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mTitles = getResources().getStringArray(R.array.titles_array);
+		mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView)findViewById(R.id.left_drawer);
+		
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+				R.layout.drawer_list_item,mTitles));
+		actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+                );
+ 
+        // Set actionBarDrawerToggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+ 
+        getActionBar().setDisplayHomeAsUpEnabled(true); 
+ 
+       
+		
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	@Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+         actionBarDrawerToggle.syncState();
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+ 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+ 
+         // call ActionBarDrawerToggle.onOptionsItemSelected(), if it returns true
+        // then it has handled the app icon touch event
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+ 
+        }
+    }
+	private void selectItem(int position) {
+		Fragment fragment = null;
+		
+		Bundle args = new Bundle();
+		switch(position){
+		case 0:
+			fragment = new HomeFragment();
+			break;
+		case 1:
+			fragment = new ProfileFragment();
+			break;
+		}
+		FragmentManager fragmentManager = getFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+		fragmentTransaction.replace(R.id.content_frame, fragment).commit();
+		
+		mDrawerList.setItemChecked(position, true);
+		setTitle(mTitles[position]);
+		mDrawerLayout.closeDrawer(mDrawerList);
+	}
+	
+	@Override
+	public void setTitle(CharSequence title) {
+		getActionBar().setTitle(title);
+	}
+
+}
